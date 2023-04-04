f4eeffd27718436e346021ee2fc94756a8f0a86c
Lukáš Poláček <lukas@polacek.email>
2023-04-04 14:57:45 +0200

    Drop the asynchronous observer
    It didn't add much value as we were waiting synchronously anyway

--- src/yaroc/utils/udev_si.py
+++ src/yaroc/utils/udev_si.py
@@ -1,5 +1,4 @@
 import logging
-import queue
 import threading
 from typing import Callable
 
@@ -41,69 +40,60 @@ class UdevSIManager:
 
     def __init__(self, worker_fn: Callable[[SIReader], None]):
         context = pyudev.Context()
-        monitor = pyudev.Monitor.from_netlink(context)
-        monitor.filter_by("tty")
-        observer = pyudev.MonitorObserver(monitor, self._handle_udev_event)
-        observer.start()
+        self.monitor = pyudev.Monitor.from_netlink(context)
+        self.monitor.filter_by("tty")
+        self.si_workers = {}
         self.worker_fn = worker_fn
-        self.queue = queue.Queue()
         for device in context.list_devices():
             self._handle_udev_event("add", device)
 
-    def loop(self):
-        si_workers = {}
-        while True:
-            (action, device_node) = self.queue.get()
-            if action == "add":
-                if device_node in si_workers:
-                    continue
-                logging.info(f"Inserted SportIdent device {device_node}")
-                try:
-                    si = SIReaderReadout(device_node)
-                    is_control = False
-                    if si.get_type() == SIReader.M_SRR:
-                        is_control = True
-                        si.disconnect()
-                        si = SIReaderSRR(device_node)
-                    elif (
-                        si.get_type() == SIReader.M_CONTROL
-                        or si.get_type() == SIReader.M_BC_CONTROL
-                    ):
-                        is_control = True
-                        si.disconnect()
-                        si = SIReaderControl(device_node)
-
-                    if is_control:
-                        si_workers[device_node] = SiWorker(si, self.worker_fn)
-                        logging.info(f"Connected to {si.port}")
-                    else:
-                        logging.warn(
-                            f"Station {si.port} not an SRR dongle or not set in autosend mode"
-                        )
-
-                except Exception as err:
-                    logging.error(f"Failed to connect to an SI station at {device_node}: {err}")
-            elif action == "remove":
-                if device_node in si_workers:
-                    logging.info(f"Removed device {device_node}")
-                    si_worker = si_workers[device_node]
-                    si_worker.close()
-                    del si_workers[device_node]
-
-    def _handle_udev_event(self, action, device: pyudev.Device):
+    def _is_sportident(self, device: pyudev.Device):
         try:
             is_sportident = (
                 device.subsystem == "tty"
                 and device.properties["ID_USB_VENDOR_ID"] == "10c4"
                 and device.properties["ID_MODEL_ID"] == "800a"
             )
+            return is_sportident
         except Exception:
             # pyudev sucks, it throws an exception when you're only doing a lookup
-            is_sportident = False
+            return False
 
-        if is_sportident:
-            device_node = device.device_node
-            if action == "add":
-                self.queue.put((action, device_node))
-            elif action == "remove":
-                self.queue.put((action, device_node))
+    def loop(self):
+        for device in iter(self.monitor.poll, None):
+            self._handle_udev_event(device.action, device)
+
+    def _handle_udev_event(self, action, device: pyudev.Device):
+        if not self._is_sportident(device):
+            return
+        device_node = device.device_node
+        if device.action == "add":
+            if device_node in self.si_workers:
+                return
+            logging.info(f"Inserted SportIdent device {device_node}")
+            try:
+                si = SIReaderReadout(device_node)
+                is_control = False
+                if si.get_type() == SIReader.M_SRR:
+                    is_control = True
+                    si.disconnect()
+                    si = SIReaderSRR(device_node)
+                elif si.get_type() == SIReader.M_CONTROL or si.get_type() == SIReader.M_BC_CONTROL:
+                    is_control = True
+                    si.disconnect()
+                    si = SIReaderControl(device_node)
+
+                if is_control:
+                    self.si_workers[device_node] = SiWorker(si, self.worker_fn)
+                    logging.info(f"Connected to {si.port}")
+                else:
+                    logging.warn(f"Station {si.port} not an SRR dongle or not set in autosend mode")
+
+            except Exception as err:
+                logging.error(f"Failed to connect to an SI station at {device_node}: {err}")
+        elif device.action == "remove":
+            if device_node in self.si_workers:
+                logging.info(f"Removed device {device_node}")
+                si_worker = self.si_workers[device_node]
+                si_worker.close()
+                del self.si_workers[device_node]
