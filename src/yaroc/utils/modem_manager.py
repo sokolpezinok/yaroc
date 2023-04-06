ebb478d9b307655a7503fcadcb114a5fee6cac76
Lukáš Poláček <lukas@polacek.email>
2023-04-06 14:55:06 +0200

    Migrate to pydbus

--- src/yaroc/utils/modem_manager.py
+++ src/yaroc/utils/modem_manager.py
@@ -1,44 +1,35 @@
-# TODO: also check https://lazka.github.io/pgi-docs/ModemManager-1.0/index.html
 import logging
-from typing import List
 
-import dbus
+from gi.repository import GLib
+from pydbus import SystemBus
 
-MODEM_MANAGER = "org.freedesktop.ModemManager1"
+MODEM_MANAGER = ".ModemManager1"
 
 
 class ModemManager:
     def __init__(self):
-        self.bus = dbus.SystemBus()
-        self.modem_manager = self.bus.get_object(MODEM_MANAGER, "/org/freedesktop/ModemManager1")
+        self.bus = SystemBus()
+        MODEM_MANAGER_PATH = "/org/freedesktop/ModemManager1"
+        self.modem_manager = self.bus.get(MODEM_MANAGER, MODEM_MANAGER_PATH)
 
-    def get_modems(self) -> List[dbus.ObjectPath]:
+    def get_modems(self) -> list[str]:
         # TODO: add filtering options
-        object_interface = dbus.Interface(self.modem_manager, "org.freedesktop.DBus.ObjectManager")
-        modems = []
-        for p in object_interface.GetManagedObjects():
-            if isinstance(p, dbus.ObjectPath):
-                modems.append(p)
-        return modems
+        return list(self.modem_manager.GetManagedObjects())
 
-    def create_sms(self, modem: dbus.ObjectPath, number: str, text: str) -> dbus.ObjectPath:
-        modem_obj = self.bus.get_object(MODEM_MANAGER, str(modem))
-        messaging_interface = dbus.Interface(
-            modem_obj, "org.freedesktop.ModemManager1.Modem.Messaging"
-        )
-        sms_path = messaging_interface.Create(
+    def create_sms(self, modem_path: str, number: str, text: str) -> str:
+        modem = self.bus.get(MODEM_MANAGER, modem_path)
+        sms_path = modem.Create(
             {
-                "text": dbus.String(text, variant_level=1),
-                "number": dbus.String(number, variant_level=1),
+                "text": GLib.Variant("s", text),
+                "number": GLib.Variant("s", number),
             }
         )
         return sms_path
 
     def send_sms(self, sms_path) -> bool:
         try:
-            sms = self.bus.get_object(MODEM_MANAGER, sms_path)
-            sms_interface = dbus.Interface(sms, "org.freedesktop.ModemManager1.Sms")
-            sms_interface.Send()
+            sms = self.bus.get(MODEM_MANAGER, sms_path)
+            sms.Send()
             return True
         except Exception as err:
             logging.error(err)
