3bb734557c2f42847683795046459e4e2cf4bd26
Lukáš Poláček <lukas@polacek.email>
2023-05-02 08:25:24 +0200

    Introduce SIM7020Interface for AT commands

--- src/yaroc/clients/mqtt.py
+++ src/yaroc/clients/mqtt.py
@@ -5,6 +5,7 @@ from threading import Lock
 from typing import Any, Dict, List, Optional, Tuple
 
 import paho.mqtt.client as mqtt
+from attila.atcommand import ATCommand
 from attila.atre import ATRuntimeEnvironment
 from attila.exceptions import ATRuntimeError, ATScriptSyntaxError, ATSerialPortError
 
@@ -120,14 +121,9 @@ class MqttClient(Client):
         return message_info
 
 
-class SIM7020MqttClient(Client):
-    """Class for an MQTT client using SIM7020's AT commands"""
-
-    # TODO: move AT commands into a separate class
-
-    def __init__(self, mac_address: str, port: str, name: Optional[str] = None):
-        self.topic_punches, self.topic_coords, self.topic_status = topics_from_mac(mac_address)
-
+class SIM7020Interface:
+    # TODO: it should be thread-safe
+    def __init__(self, port: str, client_name: str = "SIM7020"):
         self.atrunenv = ATRuntimeEnvironment(False)
         self.atrunenv.configure_communicator(port, 115200, None, "\r", rtscts=False)
         try:
@@ -136,125 +132,139 @@ class SIM7020MqttClient(Client):
         except ATSerialPortError as err:
             logging.error("Failed to open serial port")
             raise err
-
-        self._lock = Lock()
+        self._client_name = client_name
+        self._default_delay = 100
+        self._default_timeout = 1
         self._atrunenv_lock = Lock()
-        self._send_at("AT+CMEE=2;;OK;;300;;1")
-        self._send_at("ATE0;;OK;;300;;1")
-        if self._send_at("AT;;OK;;300;;1") is not None:
+
+        self._send_at("AT+CMEE=2")
+        self._send_at("ATE0")
+        self._send_at("AT+CMQTSYNC=1")
+
+        # self._disconnected = Disconnected()
+        # if name is None:
+        #     self._disconnected.client_name = ""
+        # else:
+        #     self._disconnected.client_name = str(name)
+        if self._send_at("AT", "OK") is not None:
             logging.info("SIM7020 is ready")
-
-        self._disconnected = Disconnected()
-        if name is None:
-            self._disconnected.client_name = ""
         else:
-            self._disconnected.client_name = str(name)
-        self._connect()
+            raise ATSerialPortError(
+                "Modem not responding"
+            )  # TODO: there might be a better exception
 
-    def _send_at(self, command: str) -> List[str] | None:
-        (_, opt_response) = self._send_at_queries(command, [])
-        return opt_response
-
-    def _send_at_queries(self, command: str, queries=[str]) -> Tuple[List[str], List[str] | None]:
-        def _exec(command: str):
-            try:
-                with self._atrunenv_lock:
-                    return self.atrunenv.exec(command)
-            except ATRuntimeError as err:
-                logging.error(f"Runtime error {err}")
-                raise err
-            except ATSerialPortError as err:
-                logging.error("Failed to open serial port")
-                raise err
-            except Exception as err:
-                logging.error(f"Unknown exception {err}")
-                raise err
-
-        error_res: Tuple[List[str], List[str] | None] = ([], None)
-        try:
-            response = _exec(command)
-        except Exception:
-            return error_res
-
-        if response is None:
-            logging.error("No response")
-            return error_res
-
-        command_end = command.index(";;")
-        orig_command = response.command.command
-        if len(response.full_response) == 0:
-            # TODO: this often happens before the timeout, looks like a bug in ATtila
-            response = _exec(command[command_end:])
-            if response is None:
-                logging.error("No response")
-                return error_res
-        logging.debug(f"{orig_command}: {response.full_response} {response.response}")
-        if response.response is None:
-            return ([], None)
-
-        res = []
-        for query in queries:
-            res.append(response.get_collectable(query))
-        return (res, response.full_response)
-
-    def _detect_mqtt_id(self) -> int | None:
-        (answers, full_response) = self._send_at_queries(
-            f'AT+CMQCON?;;CMQCON: [0-9],1,"{BROKER_URL}";;200;;2;;'
-            '["CMQCON: ?{mqtt_id::[0-9]},1"]',
-            ["mqtt_id"],
+    def _send_at(
+        self,
+        command: str,
+        expected: str = "OK",
+        collectables: List[str] = [],
+        queries: List[str] = [],
+        timeout: float | None = None,
+    ) -> Tuple[str, list[str]] | None:
+        timeout = timeout if timeout is not None else self._default_timeout
+        at_command = ATCommand(
+            command,
+            exp_response=expected,
+            delay=self._default_delay,
+            tout=timeout,
+            collectables=collectables,
         )
         try:
+            with self._atrunenv_lock:
+                self.atrunenv.add_command(at_command)
+                response = self.atrunenv.exec_next()
+        except ATRuntimeError as err:
+            logging.error(f"Runtime error {err}")
+            raise err
+        except ATSerialPortError as err:
+            logging.error("Failed to open serial port")
+            raise err
+        except Exception as err:
+            logging.error(f"Unknown exception {err}")
+            raise err
+
+        if response is None:
+            return None
+
+        logging.debug(f"{command}: {response.full_response} {response.response}")
+        if response.response is None:
+            return None
+        # if len(response.full_response) == 0:
+        #     # TODO: this often happens before the timeout, looks like a bug in ATtila
+        #     response = _exec(command[command_end:])
+        #     if response is None:
+        #         logging.error("No response")
+        #         return None
+
+        answers = []
+        for query in queries:
+            answers.append(response.get_collectable(query))
+        return (response.response, answers)
+
+    def __del__(self):
+        mqtt_id = self._detect_mqtt_id()
+        if mqtt_id is not None:
+            self.mqtt_disconnect(mqtt_id)
+
+    def mqtt_disconnect(self, mqtt_id: int | None):
+        if mqtt_id is not None:
+            self._send_at(f"AT+CMQDISCON={mqtt_id}", "OK")
+
+    def _detect_mqtt_id(self) -> int | None:
+        try:
+            (_, answers) = self._send_at(
+                "AT+CMQCON?",
+                f'CMQCON: [0-9],1,"{BROKER_URL}"',
+                ["CMQCON: ?{id::[0-9]},1"],
+                ["id"],
+            )
             if len(answers) == 1:
                 return int(answers[0])
             return None
         except Exception:
             return None
 
-    def __del__(self):
-        mqtt_id = self._detect_mqtt_id()
-        if mqtt_id is not None:
-            self._disconnect(mqtt_id)
-
-    def _disconnect(self, mqtt_id: int | None):
-        if mqtt_id is not None:
-            self._send_at(f"AT+CMQDISCON={mqtt_id};;OK;;200;;5")
-
-    def _connect(self) -> int | None:
-        # TODO: move elsewhere
-        # self._send_at("AT+CMQTSYNC=1;;OK;;100;;1")
+    def mqtt_connect(self) -> int | None:
         mqtt_id = self._detect_mqtt_id()
         if mqtt_id is not None:
             return mqtt_id
 
-        if self._send_at("AT+CGREG?;;CGREG: 0,1;;100;;2") is None:
+        if self._send_at("AT+CGREG?", "CGREG: 0,1") is None:
             logging.warning("Not registered yet")
             return None
 
         # Can APN be set automatically?
-        response = self._send_at('AT*MCGDEFCONT="IP","trial-nbiot.corp";;OK;;100;;35')
+        response = self._send_at('AT*MCGDEFCONT="IP","trial-nbiot.corp"', "OK", timeout=35)
         if response is None:
             logging.warning("Can not set APN")
             return None
 
-        (answers, full_response) = self._send_at_queries(
-            f"AT+CMQNEW?;;\\+CMQNEW: [0-9],1,{BROKER_URL};;100;;2;;"
-            '["CMQNEW: ?{mqtt_id::[0-9]},1"]',
+        res = self._send_at(
+            "AT+CMQNEW?",
+            f"\\+CMQNEW: [0-9],1,{BROKER_URL}",
+            ["CMQNEW: ?{mqtt_id::[0-9]},1"],
             ["mqtt_id"],
         )
-        if full_response is not None:
-            self._disconnect(int(answers[0]))
+        if res is not None:
+            self.mqtt_disconnect(int(res[1][0]))
 
-        cmqnew = f'AT+CMQNEW="{BROKER_URL}","{BROKER_PORT}",60000,100'
-        (answers, full_response) = self._send_at_queries(
-            cmqnew + ';;CMQNEW: ;;200;;35;;["CMQNEW: ?{mqtt_id::[0-9]}"]', ["mqtt_id"]
+        res = self._send_at(
+            f'AT+CMQNEW="{BROKER_URL}","{BROKER_PORT}",60000,100',
+            "CMQNEW:",
+            ["CMQNEW: ?{mqtt_id::[0-9]}"],
+            ["mqtt_id"],
+            timeout=35,
         )
+        if res is None:
+            return None
+        (_, answers) = res
         try:
             mqtt_id = int(answers[0])
             # TODO: add will flag and will from disconnected
             # status = Status()
             # status.disconnected.CopyFrom(disconnected)
             opt_reponse = self._send_at(
-                f'AT+CMQCON={mqtt_id},3,"{self._disconnected.client_name}",120,0,0;;OK;;1000;;35'
+                f'AT+CMQCON={mqtt_id},3,"{self._client_name}",120,0,0', "OK", timeout=35
             )
             if opt_reponse is not None:
                 logging.info(f"Connected to mqtt_id={mqtt_id}")
@@ -266,6 +276,37 @@ class SIM7020MqttClient(Client):
             logging.error(f"MQTT connection unsuccessful: {err}")
             return None
 
+    def mqtt_send(self, topic: str, message: bytes, qos: int = 0) -> bool:
+        if qos == 1:
+            if self._detect_mqtt_id() is None:
+                self.mqtt_connect()
+
+        message_hex = message.hex()
+        opt_response = self._send_at(
+            f'AT+CMQPUB=0,"{topic}",{qos},0,0,{len(message_hex)},"{message_hex}"', "OK", timeout=60
+        )
+        return opt_response is not None
+
+    def get_signal_dbm(self) -> int | None:
+        res = self._send_at("AT+CENG?", "CENG", ["CENG: ?{ceng::.*}"], ["ceng"])
+        try:
+            if res is not None and len(res[1]) == 1:
+                ceng_split = res[1][0].split(",")
+                return int(ceng_split[6])
+            return None
+        except Exception as err:
+            logging.error(f"Error getting signal dBm {err}")
+            return None
+
+
+class SIM7020MqttClient(Client):
+    """Class for an MQTT client using SIM7020's AT commands"""
+
+    def __init__(self, mac_address: str, port: str, name: Optional[str] = None):
+        self.topic_punches, self.topic_coords, self.topic_status = topics_from_mac(mac_address)
+        self._at_iface = SIM7020Interface(port, name if name is not None else "SIM7020")
+        self._at_iface.mqtt_connect()
+
     def send_punch(
         self,
         card_number: int,
@@ -274,17 +315,9 @@ class SIM7020MqttClient(Client):
         mode: int,
         process_time: datetime | None = None,
     ) -> mqtt.MQTTMessageInfo:
-        with self._lock:
-            if self._detect_mqtt_id() is None:
-                self._connect()
-
         punches = Punches()
         punches.punches.append(create_punch_proto(card_number, si_time, code, mode, process_time))
-        return self._send(
-            self.topic_punches,
-            punches.SerializeToString(),
-            "Punch",
-        )
+        return self._send(self.topic_punches, punches.SerializeToString(), "Punch", qos=1)
 
     def send_coords(
         self, lat: float, lon: float, alt: float, timestamp: datetime
@@ -293,27 +326,17 @@ class SIM7020MqttClient(Client):
         return self._send(self.topic_coords, coords.SerializeToString(), "GPS coordinates")
 
     def send_mini_call_home(self, mch: MiniCallHome):
-        (answers, opt_response) = self._send_at_queries(
-            'AT+CENG?;;CENG:;;100;;2;;["CENG: ?{ceng::.*}"]', ["ceng"]
-        )
-        try:
-            if opt_response is not None and len(answers) == 1:
-                ceng_split = answers[0].split(",")
-                mch.signal_dbm = int(ceng_split[6])
-        except Exception as err:
-            logging.error(f"Error getting signal dBm {err}")
+        dbm = self._at_iface.get_signal_dbm()
+        if dbm is not None:
+            mch.signal_dbm = dbm
 
         status = Status()
         status.mini_call_home.CopyFrom(mch)
         return self._send(self.topic_status, status.SerializeToString(), "MiniCallHome", qos=0)
 
-    def _send(self, topic: str, message: bytes, message_type: str, qos: int = 1):
-        message_hex = message.hex()
-        opt_response = self._send_at(
-            f'AT+CMQPUB=0,"{topic}",{qos},0,0,{len(message_hex)},"{message_hex}";;OK;;100;;45'
-        )
-        if opt_response is None:
-            # TODO: add to unsent messages if response is ERROR
-            logging.error("Message not sent: no connection")
-        else:
+    def _send(self, topic: str, message: bytes, message_type: str, qos: int = 0):
+        if self._at_iface.mqtt_send(topic, message, qos):
             logging.info(f"{message_type} sent")
+        else:
+            # TODO: add to unsent messages if response is ERROR
+            logging.error("Message not sent")
