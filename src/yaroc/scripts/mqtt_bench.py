185004256ef5e1618df8fee08cdc190f4533696e
Lukáš Poláček <lukas@polacek.email>
2023-05-04 23:36:03 +0200

    Split MiniCallHome into a separate thread

--- src/yaroc/scripts/mqtt_bench.py
+++ src/yaroc/scripts/mqtt_bench.py
@@ -1,6 +1,7 @@
 import logging
 import time
 from datetime import datetime
+from threading import Thread
 
 from ..clients.mqtt import MqttClient, SIM7020MqttClient
 from ..utils.sys_info import create_minicallhome, eth_mac_addr
@@ -16,8 +17,17 @@ assert mac_addr is not None
 # mqtt_client = MqttClient(mac_addr, name="benchmark")
 mqtt_client = SIM7020MqttClient(mac_addr, "/dev/ttyUSB0", "SIM7020")
 
+
+def mini_call_home():
+    for i in range(1000):
+        mini_call_home = create_minicallhome()
+        mqtt_client.send_mini_call_home(mini_call_home)
+        time.sleep(20)
+
+
+thread = Thread(target=mini_call_home, daemon=True)
+thread.start()
+
 for i in range(1000):
     mqtt_client.send_punch(46283, datetime.now(), (i + 1) % 1000, 18)
-    mini_call_home = create_minicallhome()
-    mqtt_client.send_mini_call_home(mini_call_home)
     time.sleep(5)
