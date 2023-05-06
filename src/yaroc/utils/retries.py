25f61645577aa4b2ba3869bedde6383836e0b96b
Lukáš Poláček <lukas@polacek.email>
2023-05-06 16:16:51 +0200

    Add BackoffBatchedRetries
    It works similarly to BackoffRetries but tries to send all messages
    queued for sending, at once.

--- src/yaroc/utils/retries.py
+++ src/yaroc/utils/retries.py
@@ -1,10 +1,12 @@
 import asyncio
 import logging
+from asyncio import Condition
 from collections.abc import Callable
 from concurrent.futures import Future, ThreadPoolExecutor
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
+from queue import Queue
 from threading import Thread
-from typing import Any, Generic, Optional, TypeVar
+from typing import Any, Generic, Optional, Tuple, TypeVar
 
 T = TypeVar("T")
 A = TypeVar("A")
@@ -73,3 +75,127 @@ class BackoffRetries(Generic[A, T]):
 
     def execute(self, fn, *args) -> Future:
         return self.executor.submit(fn, *args)
+
+
+class RetriedMessage(Generic[A, T]):
+    def __init__(self, arg: A):
+        self.processed = Condition()
+        self.published = False
+        self.returned: T | None = None
+        self.arg = arg
+
+    async def set_published(self, returned: T):
+        async with self.processed:
+            self.published = True
+            self.returned = returned
+            self.processed.notify()
+
+    async def set_not_published(self):
+        async with self.processed:
+            self.processed.notify()
+
+    def wait_for_publish(self, timeout=None):
+        timeout_time = None if timeout is None else time.time() + timeout
+        timeout_tenth = None if timeout is None else timeout / 10.0
+
+        def timed_out():
+            return False if timeout_time is None else time.time() > timeout_time
+
+        while not timed_out():
+            self.processed.wait(timeout_tenth)
+
+
+class BackoffBatchedRetries(Generic[A, T]):
+    """
+    A sender that does exponential backoff in case of failed send operations
+
+    The class is thread-safe
+    """
+
+    def __init__(
+        self,
+        send_function: Callable[[list[A]], list[T]],
+        on_publish: Callable[[Any], Any],
+        first_backoff: float,
+        multiplier: float,
+        max_duration: timedelta,
+        batch_count: int = 2,
+        workers: int = 1,
+    ):
+        self.send_function = send_function
+        self.on_publish = on_publish
+        self.first_backoff = first_backoff
+        self.max_duration = max_duration
+        self.multiplier = multiplier
+        self.batch_count = batch_count
+        self._executor = ThreadPoolExecutor(max_workers=workers)
+        self.queue: Queue[RetriedMessage] = Queue()
+
+        def start_background_loop(loop: asyncio.AbstractEventLoop) -> None:
+            asyncio.set_event_loop(loop)
+            loop.run_forever()
+
+        self._loop = asyncio.new_event_loop()
+        self._thread = Thread(target=start_background_loop, args=(self._loop,), daemon=True)
+        self._thread.start()
+
+    def _send_remaining(self) -> Tuple[list[RetriedMessage], Exception | list[T]]:
+        messages = []
+        while not self.queue.empty():
+            message = self.queue.get()
+            messages.append(message)
+            if len(messages) >= self.batch_count:
+                break
+        if len(messages) == 0:
+            return ([], [])
+        try:
+            returned = self.send_function([message.arg for message in messages])
+            return (messages, returned)
+        except Exception as err:
+            return (messages, err)
+
+    async def _poll_queue(self):
+        (messages, returned) = await self._loop.run_in_executor(
+            self._executor, self._send_remaining
+        )
+        if isinstance(returned, list):
+            for message, r in zip(messages, returned):
+                await message.set_published(r)
+        else:
+            logging.error(returned)
+            for message in messages:
+                await message.set_not_published()
+
+    async def _backoff_send(self, argument: A) -> Optional[T]:
+        deadline = datetime.now() + self.max_duration
+        cur_backoff = self.first_backoff
+        retried_message: RetriedMessage[A, T] = RetriedMessage(argument)
+
+        while datetime.now() < deadline:
+            self.queue.put(retried_message)
+            asyncio.run_coroutine_threadsafe(self._poll_queue(), self._loop)
+            async with retried_message.processed:
+                await retried_message.processed.wait()
+
+                if retried_message.published:
+                    return retried_message.returned
+
+            if datetime.now() + timedelta(seconds=cur_backoff) >= deadline:
+                cur_backoff = (deadline - datetime.now()).total_seconds()
+            logging.info(f"Retrying after {cur_backoff} seconds")
+            await asyncio.sleep(cur_backoff)
+            cur_backoff = cur_backoff * self.multiplier
+
+        logging.info(f"Message expired, args = {argument}")
+        return None
+
+    def close(self, timeout=None):
+        self._thread.join(timeout)
+
+    def send(self, argument: A) -> Future:
+        future = asyncio.run_coroutine_threadsafe(self._backoff_send(argument), self._loop)
+        logging.debug("Scheduled")  # TODO: add message ID
+        return future
+
+    def execute(self, fn, *args) -> Future:
+        return self._executor.submit(fn, *args)
--- tests/test_retries.py
+++ tests/test_retries.py
@@ -1,11 +1,12 @@
+import asyncio
 import time
 import unittest
 from datetime import datetime, timedelta
 
-from yaroc.utils.retries import BackoffRetries, BatchRetries
+from yaroc.utils.retries import BackoffBatchedRetries, BackoffRetries, RetriedMessage
 
 
-class TestRetries(unittest.TestCase):
+class TestBackoffRetries(unittest.TestCase):
     def test_backoff_retries(self):
         # Note: this is not the best test as it is non-deterministic, but the error-margin is
         # pretty wide
@@ -39,6 +40,48 @@ class TestRetries(unittest.TestCase):
             delta=0.008,
         )
         self.assertAlmostEqual(finished4.timestamp(), published4.timestamp(), delta=0.004)
+
+    def test_backoff_batched_retries(self):
+        # Note: this is not the best test as it is non-deterministic, but the error-margin is
+        # pretty wide
+        stats = {1: 0, 2: 0, 3: 0}
+
+        def send_f(xs: list[int]):
+            one_failed = False
+            for x in xs:
+                time.sleep(0.04)
+                if stats[x] < x:
+                    stats[x] += 1
+                    one_failed = True
+            if one_failed:
+                raise Exception(f"Failed {xs}")
+            return [datetime.now()] * len(xs)
+
+        b = BackoffBatchedRetries(
+            send_f, lambda x: x, 0.03, 2.0, timedelta(minutes=0.1), batch_count=2
+        )
+        start = datetime.now()
+        f3 = b.send(3)
+        time.sleep(0.002)
+        f2 = b.send(2)
+        time.sleep(0.002)
+        f1 = b.send(1)
+        finished1 = f1.result()
+        finished2 = f2.result()
+        finished3 = f3.result()
+        b.close(0.1)
+
+        self.assertAlmostEqual(finished2.timestamp(), finished1.timestamp(), delta=0.001)
+        self.assertAlmostEqual(
+            finished2.timestamp(),
+            (start + timedelta(seconds=0.385)).timestamp(),
+            delta=0.01,
+        )
+        self.assertAlmostEqual(
+            finished3.timestamp(),
+            (start + timedelta(seconds=0.445)).timestamp(),
+            delta=0.01,
+        )
 
 
 if __name__ == "__main__":
