c3f227baefd0f8aacf69cc5bb362949711d86138
Lukáš Poláček <lukas@polacek.email>
2023-05-06 16:16:51 +0200

    Remove BatchRetries

--- src/yaroc/utils/retries.py
+++ src/yaroc/utils/retries.py
@@ -2,10 +2,9 @@ import asyncio
 import logging
 from collections.abc import Callable
 from concurrent.futures import Future, ThreadPoolExecutor
-from datetime import datetime, time, timedelta
-from queue import Queue
-from threading import Condition, Thread
-from typing import Any, Generic, Optional, Tuple, TypeVar
+from datetime import datetime, timedelta
+from threading import Thread
+from typing import Any, Generic, Optional, TypeVar
 
 T = TypeVar("T")
 A = TypeVar("A")
@@ -71,74 +70,6 @@ class BackoffRetries(Generic[A, T]):
         future = asyncio.run_coroutine_threadsafe(self._backoff_send(argument), self._loop)
         logging.debug("Scheduled")  # TODO: add message ID
         return future
-
-    def execute(self, fn, *args) -> Future:
-        return self.executor.submit(fn, *args)
-
-
-class BatchMessageInfo:
-    def __init__(self, mid: int):
-        self._published = False
-        self._condition = Condition()
-        self.mid = mid
-
-    def set_as_published(self):
-        with self._condition:
-            self._published = True
-            self._condition.notify_all()
-
-    def wait_for_publish(self, timeout=None):
-        timeout_time = None if timeout is None else time.time() + timeout
-        timeout_tenth = None if timeout is None else timeout / 10.0
-
-        def timed_out():
-            return False if timeout_time is None else time.time() > timeout_time
-
-        with self._condition:
-            while not self._published and not timed_out():
-                self._condition.wait(timeout_tenth)
-
-
-class BatchRetries(Generic[A, T]):
-    # TODO: add callback and max_duration
-    def __init__(self, send_function: Callable[[list[A]], T], batch_count: int = 1):
-        self.queue: Queue[Tuple[A, BatchMessageInfo]] = Queue()
-        self.send_function = send_function
-        self.executor = ThreadPoolExecutor(max_workers=1)
-        self.batch_count = batch_count
-        self.message_counter = 0
-
-    def _send_remaining(self):
-        messages, message_infos = [], []
-        while not self.queue.empty():
-            message, message_info = self.queue.get()
-            messages.append(message)
-            message_infos.append(message_info)
-            if len(messages) >= self.batch_count:
-                break
-        if len(messages) == 0:
-            return
-
-        try:
-            self.send_function(messages)
-            for message_info in message_infos:
-                message_info.set_as_published()
-        except Exception:
-            pass
-            # TODO: this is too agressive without a backoff or throttling
-            # for message, message_info in zip(messages, message_infos):
-            #     self.queue.put((message, message_info))
-
-    def _send_all(self, _):
-        if not self.queue.empty():
-            self.executor.submit(self._send_remaining).add_done_callback(self._send_all)
-
-    def send(self, arg: A) -> BatchMessageInfo:
-        ret = BatchMessageInfo(self.message_counter)
-        self.message_counter += 1
-        self.queue.put((arg, ret))
-        self.executor.submit(self._send_remaining).add_done_callback(self._send_all)
-        return ret
 
     def execute(self, fn, *args) -> Future:
         return self.executor.submit(fn, *args)
--- tests/test_retries.py
+++ tests/test_retries.py
@@ -40,51 +40,6 @@ class TestRetries(unittest.TestCase):
         )
         self.assertAlmostEqual(finished4.timestamp(), published4.timestamp(), delta=0.004)
 
-    # def test_batch_retries(self):
-    #     # Note: this is not the best test as it is non-deterministic, but the error-margin is
-    #     # pretty wide
-    #     failures = {1: 2, 2: 3, 3: 0}
-    #
-    #     def send_f(messages: list[int]):
-    #         one_failed = False
-    #         for message in messages:
-    #             time.sleep(0.1)
-    #             if failures[message] > 0:
-    #                 one_failed = True
-    #             failures[message] -= 1
-    #         if one_failed:
-    #             raise Exception("W")
-    #
-    #     b = BatchRetries(send_f, 2)
-    #     start = datetime.now()
-    #     z1 = b.send(1)
-    #     time.sleep(0.05)
-    #     z2 = b.send(2)
-    #     time.sleep(0.05)
-    #     z3 = b.send(3)
-    #     z1.wait_for_publish()
-    #     finished1 = datetime.now()
-    #     z3.wait_for_publish()
-    #     finished3 = datetime.now()
-    #     z2.wait_for_publish()
-    #     finished2 = datetime.now()
-    #
-    #     self.assertAlmostEqual(
-    #         finished1.timestamp(),
-    #         finished3.timestamp(),
-    #         delta=0.005,
-    #     )
-    #     self.assertAlmostEqual(
-    #         finished1.timestamp(),
-    #         (start + timedelta(seconds=0.7)).timestamp(),
-    #         delta=0.02,
-    #     )
-    #     self.assertAlmostEqual(
-    #         finished2.timestamp(),
-    #         (start + timedelta(seconds=0.9)).timestamp(),
-    #         delta=0.02,
-    #     )
-
 
 if __name__ == "__main__":
     unittest.main()
