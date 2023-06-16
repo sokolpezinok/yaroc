6d7d8074a02b9385513a35878dedd1449b15b8d0
Lukáš Poláček <lukas@polacek.email>
2023-06-16 16:43:20 +0200

    Increase sending deadline

--- src/yaroc/clients/mqtt.py
+++ src/yaroc/clients/mqtt.py
@@ -140,7 +140,7 @@ class SIM7020MqttClient(Client):
         self._include_sending_timestamp = False
 
         self._retries = BackoffBatchedRetries(
-            self._send_punches, 3.0, 2.0, timedelta(minutes=10), retry_loop, batch_count=4
+            self._send_punches, 3.0, 2.0, timedelta(minutes=45), retry_loop, batch_count=4
         )
 
     def _handle_registration(self, line: str):
