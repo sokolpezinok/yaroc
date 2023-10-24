a9c62020fcc71c698a551e1f55bf8ede8ba36167
Lukáš Poláček <lukas@polacek.email>
2023-10-24 09:26:04 +0200

    Fix test_clockTest clock

--- tests/test_clock.py
+++ tests/test_clock.py
@@ -1,15 +1,15 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timezone
 
 from yaroc.utils.sys_info import is_time_off
 
 
-class TestMeos(unittest.TestCase):
+class TestClock(unittest.TestCase):
     def test_competitor_parsing(self):
         modem_clock = "23/06/09,12:06:31+08"
-        now = datetime(2023, 6, 9, 14, 6, 25)
+        now = datetime(2023, 6, 9, 12, 6, 25, tzinfo=timezone.utc)
         self.assertEqual(
-            is_time_off(modem_clock, now), datetime(2023, 6, 9, 14, 6, 31).astimezone()
+            is_time_off(modem_clock, now), datetime(2023, 6, 9, 12, 6, 31, tzinfo=timezone.utc)
         )
-        now = datetime(2023, 6, 9, 14, 6, 27)
+        now = datetime(2023, 6, 9, 12, 6, 27, tzinfo=timezone.utc)
         self.assertEqual(is_time_off(modem_clock, now), None)
