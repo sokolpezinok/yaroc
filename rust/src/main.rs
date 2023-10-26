de6874eff8ff263d6a8ab30bb106fe633a6e2d4d
Lukáš Poláček <lukas@polacek.email>
2023-10-26 11:41:32 +0200

    Improve AsyncSerial
    Also add call_without_match function.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,12 +5,15 @@ use tokio_serial::{SerialPortBuilderExt, SerialStream};
 
 struct AsyncSerial {
     serial: tokio::sync::Mutex<SerialStream>,
+    buffer_size: usize,
 }
 
 #[derive(thiserror::Error, Debug)]
 enum AsyncSerialError {
     #[error("Timed out")]
     Timeout(f64), // timeout in seconds
+    #[error("Modem error")]
+    ModemError,
     #[error("Serial read error")]
     ReadError(Box<dyn Error>),
     #[error("Serial write error")]
@@ -23,6 +26,7 @@ impl AsyncSerial {
         let serial = builder.open_native_async()?;
         Ok(Self {
             serial: tokio::sync::Mutex::new(serial),
+            buffer_size: 1024,
         })
     }
 
@@ -34,8 +38,23 @@ impl AsyncSerial {
         timeout: f64,
     ) -> Result<HashMap<String, String>, AsyncSerialError> {
         let response = self.call_with_timeout(command, timeout).await?;
+        if response.len() == 0 || response.last().unwrap() == "ERROR" {
+            return Err(AsyncSerialError::ModemError);
+        }
         let query = Self::search(response, matc, queries);
         Ok(query)
+    }
+
+    pub async fn call_without_match(
+        &mut self,
+        command: &str,
+        timeout: f64,
+    ) -> Result<(), AsyncSerialError> {
+        let response = self.call_with_timeout(command, timeout).await?;
+        if response.len() == 0 || response.last().unwrap() == "ERROR" {
+            return Err(AsyncSerialError::ModemError);
+        }
+        Ok(())
     }
 
     async fn call_with_timeout(
@@ -43,16 +62,17 @@ impl AsyncSerial {
         command: &str,
         timeout: f64,
     ) -> Result<Vec<String>, AsyncSerialError> {
+        // TODO: read what's left in the read buffer in the beginning
         let mut serial = self.serial.lock().await;
         serial
             .write(format!("{command}\r\n").as_bytes())
             .await
             .map_err(|e| AsyncSerialError::WriteError(Box::new(e)))?;
 
-        let mut buffer = Vec::with_capacity(256);
+        let mut buffer = Vec::with_capacity(self.buffer_size);
         let result = tokio::time::timeout(
             Duration::from_micros((timeout * 1_000_000.0).trunc() as u64),
-            serial.read_buf(&mut buffer),
+            serial.read_buf(&mut buffer), // TODO: this should stop on OK or ERROR
         )
         .await;
         std::mem::drop(serial);
@@ -65,7 +85,7 @@ impl AsyncSerial {
                     Ok(buffer
                         .split("\r\n")
                         .filter(|line| !line.is_empty())
-                        .map(|line| line.to_owned())
+                        .map(|line| line.trim_end().to_owned())
                         .collect())
                 }
                 Err(e) => Err(AsyncSerialError::ReadError(Box::new(e))),
