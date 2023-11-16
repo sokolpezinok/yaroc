ebdf5b57b6d890b18399005c955fa7b6e8b726f6
Lukáš Poláček <lukas@polacek.email>
2023-11-16 08:44:31 +0100

    Rename the Rust python module to yaroc_rs

--- rust/src/python.rs
+++ rust/src/python.rs
@@ -6,11 +6,8 @@ pub fn sportident_checksum(message: &[u8]) -> Vec<u8> {
     chksum.to_le_bytes().into_iter().collect()
 }
 
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
-pub fn yaroc(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+pub fn yaroc_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sportident_checksum, m)?)?;
     Ok(())
 }
