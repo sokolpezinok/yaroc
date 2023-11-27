ba0ee46984e951332be1bb69210b8260c2915c38
Lukáš Poláček <lukas@polacek.email>
2023-11-27 15:01:56 +0500

    Fix timestamp_to_datetime

--- rust/src/python.rs
+++ rust/src/python.rs
@@ -1,12 +1,20 @@
 use std::borrow::Cow;
 
-use chrono::{FixedOffset, NaiveDateTime};
+use chrono::{DateTime, FixedOffset, NaiveDateTime};
 use pyo3::prelude::*;
 
 #[pyfunction]
 pub fn sportident_checksum(message: &[u8]) -> Vec<u8> {
     let chksum = crate::punch::sportident_checksum(message);
     chksum.to_le_bytes().into_iter().collect()
+}
+
+fn timestamp_to_datetime(timestamp: f64, tz_offset_secs: i32) -> Option<DateTime<FixedOffset>> {
+    let secs = timestamp as i64;
+    let nanos = ((timestamp - secs as f64) * 1e9) as u32;
+    let tz = FixedOffset::east_opt(tz_offset_secs).unwrap();
+    NaiveDateTime::from_timestamp_opt(secs, nanos)
+        .map(|time| DateTime::from_naive_utc_and_offset(time, tz))
 }
 
 #[pyfunction]
@@ -17,17 +25,11 @@ pub fn punch_to_bytes<'a>(
     tz_offset_secs: f64,
     mode: u8,
 ) -> Cow<'a, [u8]> {
-    let secs = timestamp as i64;
-    let nanos = ((timestamp - secs as f64) * 1e9) as u32;
-    let tz = FixedOffset::east_opt(tz_offset_secs.round() as i32).unwrap();
-    match NaiveDateTime::from_timestamp_opt(secs, nanos) {
+    match timestamp_to_datetime(timestamp, tz_offset_secs.round() as i32) {
         None => [0; 20].into_iter().collect(),
-        Some(time) => {
-            let time_tz = time.and_local_timezone(tz).unwrap();
-            crate::punch::punch_to_bytes(code, time_tz, card, mode)
-                .into_iter()
-                .collect()
-        }
+        Some(time) => crate::punch::punch_to_bytes(code, time, card, mode)
+            .into_iter()
+            .collect(),
     }
 }
 
@@ -36,4 +38,15 @@ pub fn yaroc_rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sportident_checksum, m)?)?;
     m.add_function(wrap_pyfunction!(punch_to_bytes, m)?)?;
     Ok(())
+}
+
+mod test_conversion {
+    use super::timestamp_to_datetime;
+
+    #[test]
+    fn test_timestamp() {
+        let time = timestamp_to_datetime(24. * 60. * 60. + 123., 3600).unwrap();
+        let str = format!("{time}");
+        assert_eq!(str, "1970-01-02 01:02:03 +01:00");
+    }
 }
