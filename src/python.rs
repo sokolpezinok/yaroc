886bfabc1b668d95231f11290fb4d28b4ef91aa8
Lukáš Poláček <lukas@polacek.email>
2023-11-29 01:06:45 +0500

    Remove sportident_checksum from Python interface

--- src/python.rs
+++ src/python.rs
@@ -1,14 +1,7 @@
 use pyo3::prelude::*;
 
-#[pyfunction]
-pub fn sportident_checksum(message: &[u8]) -> Vec<u8> {
-    let chksum = crate::punch::sportident_checksum(message);
-    chksum.to_le_bytes().into_iter().collect()
-}
-
 #[pymodule]
 pub fn rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sportident_checksum, m)?)?;
     m.add_class::<crate::punch::SiPunch>()?;
     Ok(())
 }
