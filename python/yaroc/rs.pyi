3dce5c2082e748f7204561e700826393e060925a
Lukáš Poláček <lukas@polacek.email>
2023-12-02 23:06:58 +0100

    Add SiPunch type annotations

---
+++ python/yaroc/rs.pyi
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import ClassVar as _ClassVar
+
+class SiPunch(object):
+    CARD_FIELD_NUMBER: _ClassVar[int]
+    CODE_FIELD_NUMBER: _ClassVar[int]
+    TIME_FIELD_NUMBER: _ClassVar[int]
+    MODE_FIELD_NUMBER: _ClassVar[int]
+    card: int
+    code: int
+    time: datetime
+    mode: int
+
+    @staticmethod
+    def new(card: int, code: int, time: datetime, mode: int) -> "SiPunch": ...
+    @staticmethod
+    def from_raw(payload: bytes) -> "SiPunch": ...
