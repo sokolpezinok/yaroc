0793fff3f349cb5850b8d00f777acaf24deecea1
Lukáš Poláček <lukas@polacek.email>
2023-12-22 01:52:47 +0100

    Add geo_distance function

--- src/python.rs
+++ src/python.rs
@@ -1,7 +1,20 @@
-use pyo3::prelude::*;
+use geoutils::Location;
+use pyo3::{exceptions::PyRuntimeError, prelude::*};
+
+fn distance(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> Result<f64, String> {
+    let a = Location::new(lat1, lon1);
+    let b = Location::new(lat2, lon2);
+    Ok(a.distance_to(&b)?.meters())
+}
+
+#[pyfunction]
+pub fn geo_distance(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> PyResult<f64> {
+    distance(lat1, lon1, lat2, lon2).map_err(|e| PyRuntimeError::new_err(e))
+}
 
 #[pymodule]
 pub fn rs(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<crate::punch::SiPunch>()?;
+    m.add_function(wrap_pyfunction!(geo_distance, m)?)?;
     Ok(())
 }
