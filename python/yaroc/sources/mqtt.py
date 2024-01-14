7e66ec70e71affdf34b8199632bd6bfe273c138e
Lukáš Poláček <lukas@polacek.email>
2024-01-14 21:11:32 +0100

    Cleanup MqttForwader

--- python/yaroc/sources/mqtt.py
+++ python/yaroc/sources/mqtt.py
@@ -116,6 +116,7 @@ class MqttForwader:
                 si_punch = SiPunch.from_raw(punch.raw, mac_addr)
             except Exception as err:
                 logging.error(f"Error while constructing SiPunch: {err}")
+                continue
 
             roc_status.punch(si_punch.time, si_punch.code)
             if punches.HasField("sending_timestamp"):
@@ -124,28 +125,31 @@ class MqttForwader:
                 send_time = None
             await self._process_punch(si_punch, mac_addr, now, send_time)
 
+    @staticmethod
+    def meshtastic_mac(service_envelop: ServiceEnvelope) -> str:
+        node_id = getattr(service_envelop.packet, "from")
+        return f"{node_id:08x}"
+
     async def _handle_meshtastic_serial(self, payload: PayloadType, now: datetime):
         try:
-            se = ServiceEnvelope.FromString(MqttForwader._payload_to_bytes(payload))
+            service_envelope = ServiceEnvelope.FromString(MqttForwader._payload_to_bytes(payload))
         except Exception as err:
             logging.error(f"Error while parsing protobuf: {err}")
             return
-        if not se.packet.HasField("decoded"):
+        packet = service_envelope.packet
+        if not packet.HasField("decoded"):
             logging.error("Encrypted message! Disable encryption for meshtastic MQTT")
             return
-        if se.packet.decoded.portnum != SERIAL_APP:
-            logging.debug(f"Ignoring message with portnum {se.packet.decoded.portnum}")
+        if packet.decoded.portnum != SERIAL_APP:
+            logging.debug(f"Ignoring message with portnum {packet.decoded.portnum}")
             return
-        node_id = getattr(se.packet, "from")
-        mac_addr = f"{node_id:08x}"
 
+        mac_addr = MqttForwader.meshtastic_mac(service_envelope)
         try:
-            punch = SiPunch.from_raw(se.packet.decoded.payload, mac_addr)
+            punch = SiPunch.from_raw(packet.decoded.payload, mac_addr)
             await self._process_punch(punch, mac_addr, now, override_mac=self.meshtastic_mac)
         except Exception as err:
-            logging.error(
-                f"Cannot construct SiPunch from {mac_addr} {se.packet.decoded.payload!r}: {err}"
-            )
+            logging.error(f"Cannot parse SiPunch from {mac_addr} {packet.decoded.payload!r}: {err}")
 
     async def _handle_status(self, mac_addr: str, payload: PayloadType, now: datetime):
         try:
@@ -194,38 +198,42 @@ class MqttForwader:
                 telemetry = Telemetry.FromString(se.packet.decoded.payload)
                 if not telemetry.HasField("device_metrics"):
                     return
-                orig_time = datetime.fromtimestamp(telemetry.time).astimezone()
-                total_latency = now - orig_time
-                metrics = telemetry.device_metrics
-
-                log_message = (
-                    f"{self._resolve(mac_addr)} {orig_time:%H:%M:%S}: battery "
-                    f"{metrics.battery_level}%, {metrics.voltage:4.3f}V, "
-                    f"latency {total_latency.total_seconds():6.2f}s"
-                )
-                if se.packet.rx_rssi != 0:
-                    log_message += f", {se.packet.rx_rssi}dBm, {se.packet.rx_snr}SNR"
-                logging.info(log_message)
             except Exception as err:
                 logging.error(f"Error while constructing Telemetry: {err}")
+                return
+
+            orig_time = datetime.fromtimestamp(telemetry.time).astimezone()
+            total_latency = now - orig_time
+            metrics = telemetry.device_metrics
+
+            log_message = (
+                f"{self._resolve(mac_addr)} {orig_time:%H:%M:%S}: battery "
+                f"{metrics.battery_level}%, {metrics.voltage:4.3f}V, "
+                f"latency {total_latency.total_seconds():6.2f}s"
+            )
+            if se.packet.rx_rssi != 0:
+                log_message += f", {se.packet.rx_rssi}dBm, {se.packet.rx_snr}SNR"
+            logging.info(log_message)
         elif se.packet.decoded.portnum == POSITION_APP:
-            if se.packet.to != 4294967295:  # Request packets are ignored
+            if se.packet.to != 2**32 - 1:  # Request packets are ignored
                 return
             try:
                 position = Position.FromString(se.packet.decoded.payload)
-                orig_time = datetime.fromtimestamp(position.time).astimezone()
-                total_latency = now - orig_time
-                lat, lon = position.latitude_i / 10**7, position.longitude_i / 10**7
-
-                log_message = (
-                    f"{self._resolve(mac_addr)} {orig_time:%H:%M:%S}: lat {lat}, lon {lon}, "
-                    f"latency {total_latency.total_seconds():6.2f}s"
-                )
-                if se.packet.rx_rssi != 0:
-                    log_message += f", {se.packet.rx_rssi}dBm, {se.packet.rx_snr}SNR"
-                logging.info(log_message)
             except Exception as err:
                 logging.error(f"Error while constructing Position: {err}")
+                return
+
+            orig_time = datetime.fromtimestamp(position.time).astimezone()
+            total_latency = now - orig_time
+            lat, lon = position.latitude_i / 10**7, position.longitude_i / 10**7
+
+            log_message = (
+                f"{self._resolve(mac_addr)} {orig_time:%H:%M:%S}: lat {lat}, lon {lon}, "
+                f"latency {total_latency.total_seconds():6.2f}s"
+            )
+            if se.packet.rx_rssi != 0:
+                log_message += f", {se.packet.rx_rssi}dBm, {se.packet.rx_snr}SNR"
+            logging.info(log_message)
         elif se.packet.decoded.portnum == RANGE_TEST_APP:
             if se.packet.rx_rssi == 0:
                 return
