79e800289d738c5ff7d67f6d129fc91ff4032802
Lukáš Poláček <lukas@polacek.email>
2024-11-03 17:35:24 +0100

    Refactor everything under Device

--- nrf52840/src/main.rs
+++ nrf52840/src/main.rs
@@ -3,7 +3,11 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_nrf::{bind_interrupts, peripherals, uarte};
+use embassy_nrf::{
+    bind_interrupts,
+    peripherals::{self, TIMER0, UARTE1},
+    uarte::{self, UarteRxWithIdle},
+};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -11,11 +15,22 @@ bind_interrupts!(struct Irqs {
     UARTE1 => uarte::InterruptHandler<peripherals::UARTE1>;
 });
 
+struct Device<'a> {
+    rx: UarteRxWithIdle<'a, UARTE1, TIMER0>,
+}
+
+impl Device<'_> {
+    pub fn new() -> Self {
+        let p = embassy_nrf::init(Default::default());
+        let uart1 = uarte::Uarte::new(p.UARTE1, Irqs, p.P0_15, p.P0_16, Default::default());
+        let (_tx, rx) = uart1.split_with_idle(p.TIMER0, p.PPI_CH0, p.PPI_CH1);
+        Self { rx }
+    }
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
-    let uart1 = uarte::Uarte::new(p.UARTE1, Irqs, p.P0_15, p.P0_16, Default::default());
-    let (_tx, _rx) = uart1.split_with_idle(p.TIMER0, p.PPI_CH0, p.PPI_CH1);
+    let device = Device::new();
     info!("uarte initialized!");
 
     loop {
