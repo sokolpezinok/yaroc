819f65b4251700def5fde2ec4b98c19b19b62e15
Lukáš Poláček <lukas@polacek.email>
2024-11-03 18:00:31 +0100

    Add BG77 modem

--- nrf52840/src/main.rs
+++ nrf52840/src/main.rs
@@ -5,7 +5,7 @@ use defmt::*;
 use embassy_executor::Spawner;
 use embassy_nrf::bind_interrupts;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
-use embassy_nrf::peripherals::{P1_03, P1_04, TIMER0, UARTE1};
+use embassy_nrf::peripherals::{P0_17, P1_03, P1_04, TIMER0, UARTE1};
 use embassy_nrf::uarte::{self, UarteRxWithIdle};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
@@ -18,6 +18,7 @@ struct Device<'a> {
     rx1: UarteRxWithIdle<'a, UARTE1, TIMER0>,
     blue_led: Output<'a, P1_04>,
     green_led: Output<'a, P1_03>,
+    modem_pin: Output<'a, P0_17>,
 }
 
 impl Device<'_> {
@@ -27,11 +28,22 @@ impl Device<'_> {
         let (_tx1, rx1) = uart1.split_with_idle(p.TIMER0, p.PPI_CH0, p.PPI_CH1);
         let green_led = Output::new(p.P1_03, Level::Low, OutputDrive::Standard);
         let blue_led = Output::new(p.P1_04, Level::Low, OutputDrive::Standard);
+        let modem_pin = Output::new(p.P0_17, Level::Low, OutputDrive::Standard);
         Self {
             rx1,
             blue_led,
             green_led,
+            modem_pin,
         }
+    }
+
+    async fn turn_on_bg77(&mut self) {
+        self.modem_pin.set_low();
+        Timer::after_millis(1000).await;
+        self.modem_pin.set_high();
+        Timer::after_millis(2000).await;
+        self.modem_pin.set_low();
+        Timer::after_millis(1000).await;
     }
 }
 
