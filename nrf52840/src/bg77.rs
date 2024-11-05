5855cef34c96051a508849072c95c3c55c15ef74
Lukáš Poláček <lukas@polacek.email>
2024-11-05 01:32:18 +0100

    Fill in timeouts from the docs

--- nrf52840/src/bg77.rs
+++ nrf52840/src/bg77.rs
@@ -17,48 +17,54 @@ impl<'a> BG77<'a> {
     }
 
     pub async fn experiment(&mut self) {
-        //device.call_uart1("ATI", 10).await.unwrap();
+        let pkt_timeout = 45_000;
+        let pkt_timeout_retry = pkt_timeout * 2;
+        self.uart1.call("AT+CMEE=2", 10).await.unwrap();
         self.uart1.call("AT+CGATT=1", 10).await.unwrap();
         self.uart1.call("AT+CEREG=2", 10).await.unwrap();
-        self.uart1
-            .call("AT+CGDCONT=1,\"IP\",trial-nbiot.corp", 1000)
-            .await
-            .unwrap();
-        //self.uart1.call_self.uart1("AT+CGDCONT?", 1000).await.unwrap();
+        //self.uart1
+        //    .call("AT+CGDCONT=1,\"IP\",trial-nbiot.corp", 1000)
+        //    .await
+        //    .unwrap();
+        self.uart1.call("AT+CGPADDR=1", 10).await.unwrap();
         self.uart1.call("AT+CEREG?", 10).await.unwrap();
         self.uart1.call("AT+QCSQ", 10).await.unwrap();
         self.uart1
             .call("AT+QMTOPEN=3,\"broker.emqx.io\",1883", 10)
             .await
             .unwrap();
-        let _ = self.uart1.read(10_000).await;
+        let _ = self.uart1.read(pkt_timeout).await;
 
         self.uart1.call("AT+QMTOPEN?", 100).await.unwrap();
         // Good response: +QMTOPEN: 2,"broker.emqx.io",1883
 
         info!("\nDone part 1\n");
         self.uart1
-            .call("AT+QMTCFG=\"timeout\",0,50,3,0", 10)
+            .call("AT+QMTCFG=\"timeout\",0,45,2,0", 300)
             .await
             .unwrap();
         self.uart1
-            .call("AT+QMTCONN=3,\"client-embassy\"", 10)
+            .call("AT+QMTCONN=3,\"client-embassy\"", pkt_timeout)
             .await
             .unwrap();
-        let _ = self.uart1.read(10_000).await;
+        let _ = self.uart1.read(pkt_timeout).await;
+        // +QMTCONN: 3,0,0
 
         self.uart1.call("AT+QMTCONN?", 10).await.unwrap();
-        // Good response +QMTCONN: 2,3
+        // Good response +QMTCONN: 3,3
 
         self.uart1
-            .call("AT+QMTPUBEX=3,0,0,0,\"topic/pub\",Hello from embassy", 1000)
+            .call(
+                "AT+QMTPUBEX=3,0,0,0,\"topic/pub\",Hello from embassy",
+                pkt_timeout_retry,
+            )
             .await
             .unwrap();
-        self.uart1.call("AT+QMTDISC=3", 10).await.unwrap();
-        let _ = self.uart1.read(10_000).await;
+        self.uart1.call("AT+QMTDISC=3", 300).await.unwrap();
+        let _ = self.uart1.read(300).await;
     }
 
-    async fn turn_on_bg77(&mut self) {
+    async fn turn_on(&mut self) {
         self.modem_pin.set_low();
         Timer::after_millis(1000).await;
         self.modem_pin.set_high();
