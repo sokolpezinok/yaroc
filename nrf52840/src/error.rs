e67983d0a45994d819b4f40a878cc255968b3f31
Lukáš Poláček <lukas@polacek.email>
2024-11-06 23:30:03 +0100

    Use thiserror, requires Rust 1.81

--- nrf52840/src/error.rs
+++ nrf52840/src/error.rs
@@ -1,10 +1,16 @@
 use defmt::Format;
+use thiserror::Error;
 
-#[derive(Debug, Format)]
+#[derive(Debug, Error, Format)]
 pub enum Error {
+    #[error("Buffer too small")]
     BufferTooSmallError,
+    #[error("String encoding error")]
     StringEncodingError,
+    #[error("UART read error")]
     UartReadError,
+    #[error("UART write error")]
     UartWriteError,
+    #[error("Timeout error")]
     TimeoutError,
 }
