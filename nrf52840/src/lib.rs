12e1ea581383e0535b20073883c936b28b722e03
Lukáš Poláček <lukas@polacek.email>
2024-12-01 23:44:11 +0700

    Use nrf52840 crate
    - It allows us to drop memory.x and build.rs
    - linker flags are now in .cargo/config.toml
    - unit tests through defmt-test are working

--- nrf52840/src/lib.rs
+++ nrf52840/src/lib.rs
@@ -8,3 +8,44 @@ pub mod error;
 pub mod si_uart;
 
 type Result<T> = core::result::Result<T, error::Error>;
+
+use cortex_m_semihosting::debug;
+use defmt_rtt as _;
+use panic_probe as _;
+use nrf52840_hal as _; // memory layout
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with status code 0.
+pub fn exit() -> ! {
+    loop {
+        debug::exit(debug::EXIT_SUCCESS);
+    }
+}
+
+/// Hardfault handler.
+///
+/// Terminates the application and makes a semihosting-capable debug tool exit
+/// with an error. This seems better than the default, which is to spin in a
+/// loop.
+#[cortex_m_rt::exception]
+unsafe fn HardFault(_frame: &cortex_m_rt::ExceptionFrame) -> ! {
+    loop {
+        debug::exit(debug::EXIT_FAILURE);
+    }
+}
+
+#[cfg(test)]
+#[defmt_test::tests]
+mod unit_tests {
+    use defmt::assert;
+
+    #[test]
+    fn it_works() {
+        assert!(true)
+    }
+}
