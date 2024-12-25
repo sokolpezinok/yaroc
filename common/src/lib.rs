c17d2d989de301134162e92152a4f0d6f61d8023
Lukáš Poláček <lukas@polacek.email>
2024-12-25 19:31:04 +0100

    Use sint32 for proto values that can be negative

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -15,3 +15,20 @@ pub type Result<T> = core::result::Result<T, error::Error>;
 pub type RawMutex = embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 #[cfg(not(all(target_abi = "eabihf", target_os = "none")))]
 pub type RawMutex = embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+
+#[cfg(test)]
+mod test_proto {
+    use crate::proto::MiniCallHome;
+    use femtopb::Message;
+
+    #[test]
+    fn negative_encoding() {
+        let mch = MiniCallHome {
+            signal_dbm: -1,
+            signal_snr_cb: -60,
+            ..Default::default()
+        };
+        let len = mch.encoded_len();
+        assert_eq!(len, 4);
+    }
+}
