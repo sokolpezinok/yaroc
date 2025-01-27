c3c47a0ebb81b47014bb8e5e91f2b914b64ac128
Lukáš Poláček <lukas@polacek.email>
2025-01-27 18:48:55 +0100

    Initial implementation of exponential backoff

--- nrf52840/src/mqtt.rs
+++ nrf52840/src/mqtt.rs
@@ -5,12 +5,14 @@ use crate::{
 };
 use core::{marker::PhantomData, str::FromStr};
 use defmt::{debug, error, info, warn};
-use embassy_sync::signal::Signal;
+use embassy_futures::select::{select3, Either3};
+use embassy_sync::{channel::Channel, signal::Signal};
 use embassy_time::{Duration, Instant, Timer, WithTimeout};
+use heapless::binary_heap::{BinaryHeap, Min};
 use heapless::{format, String};
-use yaroc_common::{at::response::CommandResponse, RawMutex};
+use yaroc_common::{at::response::CommandResponse, punch::RawPunch, RawMutex};
 
-const MQTT_MESSAGES: usize = 5;
+const MQTT_MESSAGES: usize = 8;
 const MQTT_CLIENT_ID: u8 = 0;
 
 static MQTT_EXTRA_TIMEOUT: Duration = Duration::from_millis(300);
@@ -22,7 +24,52 @@ pub static MQTT_URCS: [Signal<RawMutex, MqttPublishReport>; MQTT_MESSAGES + 1] =
     Signal::new(),
     Signal::new(),
     Signal::new(),
+    Signal::new(),
+    Signal::new(),
+    Signal::new(),
 ];
+pub static PUNCHES_TO_SEND: Channel<RawMutex, RawPunch, MQTT_MESSAGES> = Channel::new();
+pub static QMTPUB_URCS: Channel<RawMutex, MqttPublishReport, MQTT_MESSAGES> = Channel::new();
+
+#[derive(Clone, Copy, Eq, PartialEq, PartialOrd)]
+struct PunchMsg {
+    punch: RawPunch,
+    next_send: Instant,
+    backoff: Duration,
+    id: Option<u8>,
+}
+
+impl Default for PunchMsg {
+    fn default() -> Self {
+        Self {
+            punch: RawPunch::default(),
+            next_send: Instant::now() + Duration::from_secs(30),
+            backoff: Duration::from_secs(60),
+            id: None,
+        }
+    }
+}
+
+impl PunchMsg {
+    pub fn new(punch: RawPunch, msg_id: u8) -> Self {
+        Self {
+            punch,
+            id: Some(msg_id),
+            ..Default::default()
+        }
+    }
+
+    pub fn update_next_send(&mut self) {
+        self.next_send = Instant::now() + self.backoff;
+        self.backoff *= 2; // TODO: configurable
+    }
+}
+
+impl Ord for PunchMsg {
+    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
+        self.next_send.cmp(&other.next_send)
+    }
+}
 
 pub enum MqttPubStatus {
     Published,
@@ -278,5 +325,52 @@ impl<M: ModemHw> MqttClient<M> {
         debug!("Message ID {} successfully sent", idx);
         self.last_successful_send = Instant::now();
         Ok(())
+    }
+}
+
+#[embassy_executor::task]
+pub async fn mqtt_backoff_retries() {
+    let mut queue: BinaryHeap<PunchMsg, Min, MQTT_MESSAGES> = BinaryHeap::new();
+    let mut inflight_msgs: [PunchMsg; MQTT_MESSAGES] = [PunchMsg::default(); MQTT_MESSAGES];
+
+    loop {
+        let top = queue.peek();
+        let timer = match top {
+            None => Timer::after_secs(3600),
+            Some(msg) => Timer::at(msg.next_send),
+        };
+
+        match select3(PUNCHES_TO_SEND.receive(), QMTPUB_URCS.receive(), timer).await {
+            Either3::First(punch) => {
+                let idx = inflight_msgs.iter().position(|msg| msg.id.is_none());
+                if let Some(id) = idx {
+                    // TODO: id = 0 is special, should we use it?
+                    let msg = PunchMsg::new(punch, id as u8);
+                    inflight_msgs[id] = msg;
+                    let _ = queue.push(msg);
+                } else {
+                    error!("Message queue is full");
+                }
+            }
+            Either3::Second(qmtpub_urc) => {
+                if matches!(qmtpub_urc.status, MqttPubStatus::Timeout) {
+                    let mut msg = inflight_msgs[qmtpub_urc.msg_id as usize];
+                    if msg.id.is_some() {
+                        msg.update_next_send();
+                        let _ = queue.push(msg);
+                    } else {
+                        error!(
+                            "Gor URC for a message we don't know about, ID={}",
+                            qmtpub_urc.msg_id
+                        );
+                    }
+                }
+            }
+            Either3::Third(_) => {
+                if let Some(_punch_msg) = queue.pop() {
+                    // TODO: send punch_msg
+                }
+            }
+        }
     }
 }
