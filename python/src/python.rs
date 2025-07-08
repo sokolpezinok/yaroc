3834db8201178150865117df09338ba43dcb12cc
Lukáš Poláček <lukas@polacek.email>
2025-07-09 01:18:20 +0200

    Add CellularLog to Python

--- python/src/python.rs
+++ python/src/python.rs
@@ -64,9 +64,11 @@ pub fn rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(current_timestamp_millis, m)?)?;
 
     #[cfg(feature = "receive")]
-    m.add_class::<crate::message_handler::MessageHandler>()?;
-    #[cfg(feature = "receive")]
-    m.add_class::<crate::message_handler::MqttConfig>()?;
+    {
+        m.add_class::<crate::message_handler::MessageHandler>()?;
+        m.add_class::<crate::message_handler::MqttConfig>()?;
+        m.add_class::<crate::status::CellularLog>()?;
+    }
 
     pyo3_log::init();
     Ok(())
