a74bd8b0aa829cbea3902480055ed40a7e4d914f
Lukáš Poláček <lukas@polacek.email>
2025-10-06 08:59:11 +0200

    Drop NrfTemp

--- common/src/status.rs
+++ common/src/status.rs
@@ -14,26 +14,6 @@ pub trait Temp {
     fn cpu_temperature(&mut self) -> impl core::future::Future<Output = crate::Result<f32>>;
 }
 
-#[cfg(feature = "nrf")]
-pub struct NrfTemp {
-    temp: embassy_nrf::temp::Temp<'static>,
-}
-
-#[cfg(feature = "nrf")]
-impl NrfTemp {
-    pub fn new(temp: embassy_nrf::temp::Temp<'static>) -> Self {
-        Self { temp }
-    }
-}
-
-#[cfg(feature = "nrf")]
-impl Temp for NrfTemp {
-    async fn cpu_temperature(&mut self) -> crate::Result<f32> {
-        let temp = self.temp.read().await;
-        Ok(temp.to_num::<f32>())
-    }
-}
-
 pub static TEMPERATURE: Watch<RawMutex, f32, 1> = Watch::new();
 
 #[derive(Clone, Copy)]
