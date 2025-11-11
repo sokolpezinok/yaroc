0de02f5e6d1b2c22fc79528af7900cc82094f984
Lukáš Poláček <lukas@polacek.email>
2025-11-11 22:25:21 +0100

    Reimplement the loop from Python's SerialClient
    When tested, this Python class doesn't work, it requires tokio already
    when `SerialClient::new()` is called. We need to find a workaround.

--- python/src/serial_client.rs
+++ python/src/serial_client.rs
@@ -1,9 +1,10 @@
 use std::{future, sync::Arc};
 
-use log::info;
+use log::{error, info};
 use pyo3::exceptions::{PyConnectionError, PyRuntimeError};
 use pyo3::prelude::*;
 use pyo3_async_runtimes::tokio::future_into_py;
+use tokio::io::{AsyncBufReadExt, BufReader, BufWriter, ReadHalf, WriteHalf, split};
 use tokio::{io::AsyncWriteExt, sync::Mutex};
 use tokio_serial::{SerialPortBuilderExt, SerialStream};
 
@@ -11,19 +12,76 @@ use crate::punch::SiPunchLog;
 
 #[pyclass]
 pub struct SerialClient {
-    computer_serial: Arc<Mutex<SerialStream>>,
+    computer_rx: Arc<Mutex<BufReader<ReadHalf<SerialStream>>>>,
+    computer_tx: Arc<Mutex<BufWriter<WriteHalf<SerialStream>>>>,
 }
+
+const FIRST_RESPONSE: &[u8] = b"\xff\x02\xf0\x03\x12\x8cMb?\x03";
+const FINAL_RESPONSE: &[u8] = b"\xff\x02\x83\x83\x12\x8c\x00\r\x00\x12\x8c\x04450\x16\x0b\x0fo!\xff\xff\xff\x02\x06\x00\x1b\x17?\x18\x18\x06)\x08\x05>\xfe\n\xeb\n\xeb\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x92\xba\x1aB\x01\xff\xff\xe1\xff\xff\xff\xff\xff\x01\x01\x01\x0b\x07\x0c\x00\r]\x0eD\x0f\xec\x10-\x11;\x12s\x13#\x14;\x15\x01\x19\x1d\x1a\x1c\x1b\xc7\x1c\x00\x1d\xb0!\xb6\"\x10#\xea$\n%\x00&\x11,\x88-1.\x0b\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xf9\xc3\x03";
+const LAST_BYTE: u8 = 0x03;
 
 #[pymethods]
 impl SerialClient {
     #[new]
     pub fn new(computer_port: &str) -> PyResult<Self> {
         let builder = tokio_serial::new(computer_port, 38400);
-        let computer_serial = builder
-            .open_native_async()
-            .map_err(|e| PyConnectionError::new_err(e.to_string()))?;
+        let computer_serial = builder.open_native_async().map_err(|e| {
+            PyConnectionError::new_err(format!("Error connecting to {}: {e}", computer_port))
+        })?;
+        info!("Connected to SRR sink at {computer_port}");
+        let (rx, tx) = split(computer_serial);
+        let rx = BufReader::new(rx);
+        let tx = BufWriter::new(tx);
+
         Ok(Self {
-            computer_serial: Arc::new(Mutex::new(computer_serial)),
+            computer_rx: Arc::new(Mutex::new(rx)),
+            computer_tx: Arc::new(Mutex::new(tx)),
+        })
+    }
+
+    pub fn r#loop<'a>(&self, py: Python<'a>) -> PyResult<Bound<'a, PyAny>> {
+        let computer_rx = self.computer_rx.clone();
+        let computer_tx = self.computer_tx.clone();
+        future_into_py::<_, ()>(py, async move {
+            loop {
+                let mut rx = computer_rx.lock().await;
+                let mut query = Vec::new();
+                let _len = rx.read_until(LAST_BYTE, &mut query).await.unwrap();
+
+                match query.as_slice() {
+                    b"\xff\x02\x02\xf0\x01Mm\n\x03" => {
+                        info!("Responding to orienteering software - MeOS");
+                    }
+                    b"\xff\x02\xf0\x01Mm\n\x03" => {
+                        info!("Responding to orienteering software - SportIdent Reader");
+                    }
+                    _ => {
+                        error!("Contacted by unknown orienteering software");
+                        continue;
+                    }
+                }
+
+                let mut tx = computer_tx.lock().await;
+                let _ = tx
+                    .write_all(FIRST_RESPONSE)
+                    .await
+                    .inspect_err(|e| error!("Communication with software failed: {e}"));
+                let mut data = Vec::new();
+                let _len = rx.read_until(LAST_BYTE, &mut data).await.unwrap();
+
+                match data.as_slice() {
+                    b"\x02\x83\x02\x00\x80\xbf\x17\x03"
+                    | b"\xff\x02\x83\x02\x00\x80\xbf\x17\x03" => {}
+                    _ => {
+                        error!("Communicating with software failed");
+                        continue;
+                    }
+                }
+                let _ = tx
+                    .write_all(FINAL_RESPONSE)
+                    .await
+                    .inspect_err(|e| error!("Communication with software failed: {e}"));
+            }
         })
     }
 
@@ -32,15 +90,14 @@ impl SerialClient {
         py: Python<'a>,
         punch_log: &SiPunchLog,
     ) -> PyResult<Bound<'a, PyAny>> {
-        let computer_serial = self.computer_serial.clone();
+        let computer_tx = self.computer_tx.clone();
         let raw_punch = punch_log.punch.raw;
 
         future_into_py::<_, ()>(py, async move {
-            let mut serial = computer_serial.lock().await;
-            serial
-                .write_all(&raw_punch)
-                .await
-                .map_err(|e| PyRuntimeError::new_err(e.to_string()))?;
+            let mut tx = computer_tx.lock().await;
+            tx.write_all(&raw_punch).await.map_err(|e| {
+                PyRuntimeError::new_err(format!("Error sending punch via serial port: {e}"))
+            })?;
             info!("Punch sent via serial port");
             Ok(())
         })
