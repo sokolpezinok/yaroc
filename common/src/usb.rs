32b3e14e4338bf76ebf667c31bdd8b9db1160ac5
Lukáš Poláček <lukas@polacek.email>
2025-12-05 15:01:56 +0100

    Move UsbPacketReader under common

--- common/src/usb.rs
+++ common/src/usb.rs
@@ -1,6 +1,20 @@
+use core::future::Future;
+#[cfg(feature = "defmt")]
+use defmt::{debug, error, info, warn};
+#[cfg(feature = "nrf")]
+use embassy_nrf::usb::{Driver, vbus_detect::SoftwareVbusDetect};
+#[cfg(feature = "nrf")]
+use embassy_usb::class::cdc_acm::CdcAcmClass;
+#[cfg(not(feature = "defmt"))]
+use log::{debug, error, info, warn};
+use postcard::{from_bytes, to_vec};
 use serde::{Deserialize, Serialize};
 
 use crate::bg77::modem_manager::ModemConfig;
+use crate::error::Error;
+
+#[cfg(feature = "nrf")]
+pub type UsbDriver = Driver<'static, &'static SoftwareVbusDetect>;
 
 #[derive(Serialize, Deserialize)]
 pub enum UsbCommand {
@@ -10,4 +24,114 @@ pub enum UsbCommand {
 #[derive(Serialize, Deserialize)]
 pub enum UsbResponse {
     Ok,
+}
+
+pub trait CdcAcm {
+    fn read_packet(&mut self, buf: &mut [u8]) -> impl Future<Output = Result<usize, Error>>;
+    fn write_packet(&mut self, buf: &[u8]) -> impl Future<Output = Result<(), Error>>;
+    fn wait_connection(&mut self) -> impl Future<Output = ()>;
+}
+
+#[cfg(feature = "nrf")]
+impl CdcAcm for CdcAcmClass<'static, UsbDriver> {
+    async fn read_packet(&mut self, buf: &mut [u8]) -> Result<usize, Error> {
+        use embassy_usb::driver::EndpointError;
+        self.read_packet(buf).await.map_err(|e| match e {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Error::UsbDisconnected,
+        })
+    }
+
+    async fn write_packet(&mut self, buf: &[u8]) -> Result<(), Error> {
+        use embassy_usb::driver::EndpointError;
+        self.write_packet(buf).await.map_err(|e| match e {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Error::UsbDisconnected,
+        })
+    }
+
+    async fn wait_connection(&mut self) {
+        self.wait_connection().await
+    }
+}
+
+pub trait RequestHandler {
+    fn handle(&mut self, command: UsbCommand) -> impl Future<Output = Result<UsbResponse, Error>>;
+}
+
+const PACKET_LEN: usize = 64;
+
+pub struct UsbPacketReader<T, H> {
+    buffer: [u8; PACKET_LEN * 8],
+    class: T,
+    handler: H,
+}
+
+impl<T: CdcAcm, H: RequestHandler> UsbPacketReader<T, H> {
+    pub fn new(class: T, handler: H) -> Self {
+        Self {
+            buffer: [0; PACKET_LEN * 8],
+            class,
+            handler,
+        }
+    }
+
+    async fn read(&mut self) -> Result<&[u8], Error> {
+        let total_len = self.buffer.len();
+        let mut remaining = self.buffer.as_mut_slice();
+        loop {
+            let read_len = self.class.read_packet(remaining).await?;
+            match read_len {
+                PACKET_LEN => {
+                    remaining = &mut remaining[PACKET_LEN..];
+                }
+                len => {
+                    let len = total_len - remaining.len() + len;
+                    return Ok(&self.buffer[..len]);
+                }
+            }
+        }
+    }
+
+    async fn write(&mut self, buf: &[u8]) -> Result<(), Error> {
+        for chunk in buf.chunks(PACKET_LEN) {
+            self.class.write_packet(chunk).await.map_err(|_| Error::UsbWriteError)?;
+        }
+        // TODO: if PACKET_LEN divides buf.len(), we need to send an empty packet.
+        Ok(())
+    }
+
+    async fn respond(&mut self, command: UsbCommand) -> Result<(), Error> {
+        let response = self.handler.handle(command).await?;
+        let response_bytes = to_vec::<_, 128>(&response).map_err(|_| Error::ParseError)?;
+        self.write(response_bytes.as_slice()).await
+    }
+
+    pub async fn run(mut self) {
+        loop {
+            self.class.wait_connection().await;
+            info!("Connected to USB");
+            loop {
+                let command_result = self.read().await.and_then(|data| {
+                    debug!("Read {} bytes from USB", data.len());
+                    from_bytes::<UsbCommand>(data).map_err(|_| Error::ParseError)
+                });
+                match command_result {
+                    Ok(command) => {
+                        let _ = self
+                            .respond(command)
+                            .await
+                            .inspect_err(|_| error!("Error while responding to a USB command"));
+                    }
+                    Err(Error::UsbDisconnected) => {
+                        warn!("USB disconnected");
+                        break;
+                    }
+                    Err(e) => {
+                        error!("Error while reading from USB: {}", e);
+                    }
+                }
+            }
+        }
+    }
 }
--- nrf52840/src/usb.rs
+++ nrf52840/src/usb.rs
@@ -1,27 +1,21 @@
-use defmt::{debug, error, info, warn};
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_nrf::usb::Driver;
 use embassy_nrf::usb::vbus_detect::SoftwareVbusDetect;
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
 use embassy_usb::driver::EndpointError;
 use embassy_usb::{Builder, UsbDevice};
-use postcard::{from_bytes, to_vec};
 use static_cell::StaticCell;
 use yaroc_common::error::Error;
-use yaroc_common::usb::{UsbCommand, UsbResponse};
+use yaroc_common::usb::{
+    CdcAcm, RequestHandler, UsbCommand, UsbDriver, UsbPacketReader, UsbResponse,
+};
 
 use crate::send_punch::SEND_PUNCH_MUTEX;
-
-type UsbDriver = Driver<'static, &'static SoftwareVbusDetect>;
 
 #[embassy_executor::task]
 pub async fn usb_task(mut usb: UsbDevice<'static, UsbDriver>) {
     usb.run().await;
-}
-
-pub struct Usb {
-    device: UsbDevice<'static, UsbDriver>,
-    class: CdcAcmClass<'static, UsbDriver>,
 }
 
 static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
@@ -30,6 +24,11 @@ static CONTROL_BUF: StaticCell<[u8; 128]> = StaticCell::new();
 static MSOS_DESCRIPTOR: StaticCell<[u8; 128]> = StaticCell::new();
 static STATE: StaticCell<State<'static>> = StaticCell::new();
 const PACKET_LEN: usize = 64;
+
+pub struct Usb {
+    device: UsbDevice<'static, UsbDriver>,
+    class: CdcAcmClass<'static, UsbDriver>,
+}
 
 impl Usb {
     pub fn new(driver: UsbDriver) -> Self {
@@ -58,75 +57,17 @@ impl Usb {
 
     pub fn must_spawn(self, spawner: Spawner) {
         spawner.must_spawn(usb_task(self.device));
-        spawner.must_spawn(usb_packet_reader_loop(UsbPacketReader::from(self.class)));
+        spawner.must_spawn(usb_packet_reader_loop(UsbPacketReader::new(
+            self.class,
+            SendPunchHandler,
+        )));
     }
 }
 
-pub trait CdcAcm {
-    fn read_packet(&mut self, buf: &mut [u8]) -> impl Future<Output = crate::Result<usize>>;
-    fn write_packet(&mut self, buf: &[u8]) -> impl Future<Output = crate::Result<()>>;
-    fn wait_connection(&mut self) -> impl Future<Output = ()>;
-}
+pub struct SendPunchHandler;
 
-impl<'d, D: embassy_usb::driver::Driver<'d>> CdcAcm for CdcAcmClass<'d, D> {
-    async fn read_packet(&mut self, buf: &mut [u8]) -> crate::Result<usize> {
-        self.read_packet(buf).await.map_err(|e| match e {
-            EndpointError::BufferOverflow => panic!("Buffer overflow"),
-            EndpointError::Disabled => Error::UsbDisconnected,
-        })
-    }
-
-    async fn write_packet(&mut self, buf: &[u8]) -> crate::Result<()> {
-        self.write_packet(buf).await.map_err(|e| match e {
-            EndpointError::BufferOverflow => panic!("Buffer overflow"),
-            EndpointError::Disabled => Error::UsbDisconnected,
-        })
-    }
-
-    async fn wait_connection(&mut self) {
-        self.wait_connection().await
-    }
-}
-
-#[embassy_executor::task]
-async fn usb_packet_reader_loop(
-    usb_packet_reader: UsbPacketReader<CdcAcmClass<'static, UsbDriver>>,
-) {
-    usb_packet_reader.r#loop().await;
-}
-
-struct UsbPacketReader<T> {
-    buffer: [u8; PACKET_LEN * 8],
-    class: T,
-}
-
-impl<T: CdcAcm> UsbPacketReader<T> {
-    async fn read(&mut self) -> crate::Result<&[u8]> {
-        let total_len = self.buffer.len();
-        let mut remaining = self.buffer.as_mut_slice();
-        loop {
-            let read_len = self.class.read_packet(remaining).await?;
-            match read_len {
-                PACKET_LEN => {
-                    remaining = &mut remaining[PACKET_LEN..];
-                }
-                len => {
-                    let len = total_len - remaining.len() + len;
-                    return Ok(&self.buffer[..len]);
-                }
-            }
-        }
-    }
-
-    async fn write(&mut self, buf: &[u8]) -> crate::Result<()> {
-        for chunk in buf.chunks(PACKET_LEN) {
-            self.class.write_packet(chunk).await.map_err(|_| Error::UsbWriteError)?;
-        }
-        // TODO: if PACKET_LEN divides buf.len(), we need to send an empty packet.
-        Ok(())
-    }
-
-    async fn respond(&mut self, command: UsbCommand) -> crate::Result<()> {
+impl RequestHandler for SendPunchHandler {
+    async fn handle(&mut self, command: UsbCommand) -> Result<UsbResponse, Error> {
         let mut send_punch = SEND_PUNCH_MUTEX.lock().await;
         let send_punch = send_punch.as_mut().unwrap();
         match command {
@@ -135,44 +76,13 @@ impl<T: CdcAcm> UsbPacketReader<T> {
                 send_punch.configure_modem(modem_config).await?;
             }
         }
-        let response = to_vec::<_, 128>(&UsbResponse::Ok).unwrap();
-        self.write(response.as_slice()).await
-    }
-
-    pub async fn r#loop(mut self) {
-        loop {
-            self.class.wait_connection().await;
-            info!("Connected to USB");
-            loop {
-                let command = self.read().await.and_then(|data| {
-                    debug!("Read {} bytes from USB", data.len());
-                    from_bytes::<UsbCommand>(data).map_err(|_| Error::ParseError)
-                });
-                match command {
-                    Ok(command) => {
-                        let _ = self
-                            .respond(command)
-                            .await
-                            .inspect_err(|_| error!("Error while responding to a USB command"));
-                    }
-                    Err(Error::UsbDisconnected) => {
-                        warn!("USB disconnected");
-                        break;
-                    }
-                    Err(e) => {
-                        error!("Error while reading from USB: {}", e);
-                    }
-                }
-            }
-        }
+        Ok(UsbResponse::Ok)
     }
 }
 
-impl<T> From<T> for UsbPacketReader<T> {
-    fn from(class: T) -> Self {
-        Self {
-            buffer: [0; _],
-            class,
-        }
-    }
+#[embassy_executor::task]
+async fn usb_packet_reader_loop(
+    usb_packet_reader: UsbPacketReader<CdcAcmClass<'static, UsbDriver>, SendPunchHandler>,
+) {
+    usb_packet_reader.run().await;
 }
