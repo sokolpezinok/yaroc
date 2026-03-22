a1a3df97b7ab9b038e2b91a0a4ae0aa63df60d87
Lukáš Poláček <lukas@polacek.email>
2026-03-22 20:40:35 +0100

    Wait for confirmation of the USB command

--- python/src/yaroc_cli.rs
+++ python/src/yaroc_cli.rs
@@ -1,27 +1,46 @@
-use std::io::Write;
+use std::io::{Read, Write};
+use std::time::Duration;
 
 use clap::Parser;
 use log::{error, info};
 use postcard::to_stdvec;
 use pyo3::prelude::*;
-use yaroc_common::{bg77::modem_manager::ModemConfig, usb::UsbCommand};
+use yaroc_common::{
+    bg77::modem_manager::ModemConfig,
+    usb::{UsbCommand, UsbResponse},
+};
 
 use crate::config::{Args, Config};
 
 #[pyfunction]
 pub fn yaroc_cli() {
+    let _ = env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .format_timestamp_millis()
+        .try_init();
+
     let args = Args::parse_from(std::env::args().skip(1));
-    let builder = tokio_serial::new(&args.port, 112800);
-    let mut serial = builder.open_native().expect("Unable to open serial port");
+    let mut serial = tokio_serial::new(&args.port, 112800)
+        .timeout(Duration::from_secs(2))
+        .open_native()
+        .expect("Unable to open serial port");
 
     let config_str = std::fs::read_to_string(&args.config).expect("Unable to read config file");
     let config: Config = toml::from_str(&config_str).expect("Unable to parse config file");
 
     let modem_config: ModemConfig = config.modem.into();
     let buf = to_stdvec::<_>(&UsbCommand::ConfigureModem(modem_config)).unwrap();
-    if serial.write_all(buf.as_slice()).is_err() {
-        error!("Writing to serial failed");
+    if let Err(e) = serial.write_all(buf.as_slice()) {
+        error!("Writing to serial failed: {e}");
     } else {
         info!("Writing to serial successful");
+        let mut read_buf = [0u8; 64];
+        match serial.read(&mut read_buf) {
+            Ok(n) => match postcard::from_bytes::<UsbResponse>(&read_buf[..n]) {
+                Ok(UsbResponse::Ok) => info!("Configuration successful"),
+                Err(e) => error!("Failed to parse response: {e}"),
+            },
+            Err(e) => error!("Reading from serial failed: {e}"),
+        }
     }
 }
