98a5253e761dcc8cc75d8c413acb89f167c59ea5
Lukáš Poláček <lukas@polacek.email>
2026-03-22 10:25:19 +0100

    Make RAT deserialization more user-friendly

--- python/src/yaroc_cli.rs
+++ python/src/yaroc_cli.rs
@@ -34,10 +34,42 @@ impl From<LteBandsToml> for LteBands {
     }
 }
 
+#[derive(Debug)]
+enum RatToml {
+    Ltem,
+    NbIot,
+    LtemNbIot,
+}
+
+impl<'de> Deserialize<'de> for RatToml {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?.to_lowercase().replace("-", "");
+        match s.as_str() {
+            "ltem" => Ok(RatToml::Ltem),
+            "nbiot" => Ok(RatToml::NbIot),
+            "both" | "all" | "ltemnbiot" => Ok(RatToml::LtemNbIot),
+            _ => Err(serde::de::Error::custom(format!("unknown RAT: {}", s))),
+        }
+    }
+}
+
+impl From<RatToml> for RAT {
+    fn from(toml: RatToml) -> Self {
+        match toml {
+            RatToml::Ltem => RAT::Ltem,
+            RatToml::NbIot => RAT::NbIot,
+            RatToml::LtemNbIot => RAT::LtemNbIot,
+        }
+    }
+}
+
 #[derive(Deserialize, Debug)]
 struct ModemConfigToml {
     apn: String,
-    rat: RAT,
+    rat: RatToml,
     bands: LteBandsToml,
 }
 
@@ -45,7 +77,7 @@ impl From<ModemConfigToml> for ModemConfig {
     fn from(toml: ModemConfigToml) -> Self {
         ModemConfig {
             apn: HString::try_from(toml.apn.as_str()).unwrap_or_default(),
-            rat: toml.rat,
+            rat: toml.rat.into(),
             bands: toml.bands.into(),
         }
     }
@@ -71,5 +103,50 @@ pub fn yaroc_cli() {
         error!("Writing to serial failed");
     } else {
         info!("Writing to serial successful");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rat_deserialization() {
+        #[derive(Deserialize)]
+        struct Wrapper {
+            rat: RatToml,
+        }
+
+        let w: Wrapper = toml::from_str("rat = \"ltem\"").unwrap();
+        assert!(matches!(w.rat, RatToml::Ltem));
+
+        let w: Wrapper = toml::from_str("rat = \"NB-IoT\"").unwrap();
+        assert!(matches!(w.rat, RatToml::NbIot));
+
+        let w: Wrapper = toml::from_str("rat = \"nbiot\"").unwrap();
+        assert!(matches!(w.rat, RatToml::NbIot));
+
+        let w: Wrapper = toml::from_str("rat = \"both\"").unwrap();
+        assert!(matches!(w.rat, RatToml::LtemNbIot));
+
+        let w: Wrapper = toml::from_str("rat = \"ALL\"").unwrap();
+        assert!(matches!(w.rat, RatToml::LtemNbIot));
+    }
+
+    #[test]
+    fn test_config_deserialization() {
+        let toml_str = r#"
+            [modem]
+            apn = "test.apn"
+            rat = "both"
+            [modem.bands]
+            ltem = [1, 2, 3]
+            nbiot = [20]
+        "#;
+        let config: Config = toml::from_str(toml_str).unwrap();
+        assert_eq!(config.modem.apn, "test.apn");
+        assert!(matches!(config.modem.rat, RatToml::LtemNbIot));
+        assert_eq!(config.modem.bands.ltem, vec![1, 2, 3]);
+        assert_eq!(config.modem.bands.nbiot, vec![20]);
     }
 }
