63760dc2b2b5d0c3c1dcafef97a6fcb9bb1b06c4
Lukáš Poláček <lukas@polacek.email>
2026-05-28 17:37:04 +0200

    Use the new SerialDeviceManager in si_uart.rs example

--- receiver/examples/si_uart.rs
+++ receiver/examples/si_uart.rs
@@ -1,18 +1,10 @@
 //! An example of reading punches from a SportIdent UART device.
 
 use chrono::Local;
-use clap::Parser;
 use log::{error, info};
 
-use yaroc_common::{punch::SiPunch, si_uart::SiUart};
-use yaroc_receiver::si_uart::TokioSerial;
-
-#[derive(Parser, Debug)]
-/// Arguments for the `si_uart` example.
-struct Args {
-    #[arg(short, long)]
-    port: String,
-}
+use yaroc_common::punch::SiPunch;
+use yaroc_receiver::serial_device_manager::SerialDeviceManager;
 
 #[tokio::main]
 async fn main() {
@@ -21,16 +13,36 @@ async fn main() {
         .format_timestamp_millis()
         .init();
 
-    let args = Args::parse();
-    let rx = TokioSerial::new(&args.port).unwrap();
-    let mut si_uart = SiUart::new(rx);
-    info!("Listening for punches on {}", args.port);
-    while let Ok(punches) = si_uart.read().await {
-        let now = Local::now();
-        for punch in punches {
-            let punch = SiPunch::from_raw(punch, now.date_naive(), now.offset());
-            info!("Recieved punch: {punch:?}");
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+    let mut manager = SerialDeviceManager::new(tx);
+
+    let monitor_task = tokio::spawn(async move {
+        if let Err(e) = manager.monitor_usb_devices().await {
+            error!("Error in USB monitoring: {e}");
+        }
+    });
+
+    info!("Watching for SportIdent USB devices... Press Ctrl-C to exit.");
+    loop {
+        tokio::select! {
+            res = rx.recv() => {
+                match res {
+                    Some(punch) => {
+                        let now = Local::now();
+                        let punch = SiPunch::from_raw(punch, now.date_naive(), now.offset());
+                        info!("Received punch: {punch:?}");
+                    }
+                    None => {
+                        info!("Channel closed, shutting down...");
+                        break;
+                    }
+                }
+            }
+            _ = tokio::signal::ctrl_c() => {
+                info!("Ctrl-C received, shutting down...");
+                monitor_task.abort();
+                break;
+            }
         }
     }
-    error!("Error while receiving punch, exiting");
 }
